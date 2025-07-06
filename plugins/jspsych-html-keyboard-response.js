bd55123d8fd474ae9fd6365d324773889224a182
canyonjang <114268359+canyonjang@users.noreply.github.com>
2025-07-06 19:02:18 +0900

    Add files via upload

--- plugins/jspsych-html-keyboard-response.js
+++ plugins/jspsych-html-keyboard-response.js
@@ -1,1 +1,11 @@
-// html-keyboard-response plugin
+
+jsPsych.plugins['html-keyboard-response'] = (function() {
+  var plugin = {};
+  plugin.info = { name: 'html-keyboard-response', parameters: {} };
+  plugin.trial = function(display_element, trial) {
+    display_element.innerHTML = '<p>Sample stimulus</p>';
+    var after_response = function(info) { jsPsych.finishTrial({}); };
+    jsPsych.pluginAPI.getKeyboardResponse({callback_function: after_response});
+  };
+  return plugin;
+})();
--- plugins/jspsych-survey-text.js
+++ plugins/jspsych-survey-text.js
@@ -1,1 +1,12 @@
-// survey-text plugin
+
+jsPsych.plugins['survey-text'] = (function() {
+  var plugin = {};
+  plugin.info = { name: 'survey-text', parameters: {} };
+  plugin.trial = function(display_element, trial) {
+    display_element.innerHTML = '<input type="text" id="response"><button id="next">Next</button>';
+    document.getElementById('next').onclick = function() {
+      jsPsych.finishTrial({ response: document.getElementById('response').value });
+    };
+  };
+  return plugin;
+})();
